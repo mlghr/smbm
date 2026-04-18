3da91ca737d7adbf06787573327506432a42b589
Michael Rosenbaum <mrosenbaum.dev@gmail.com>
2026-04-17 17:26:43 -0700

    variable jump height (hold)

--- player1.gd
+++ player1.gd
@@ -1,20 +1,25 @@
 extends CharacterBody2D
 
 var speed = 600
-var jump_velocity = -625
-var gravity = 1250
+var jump_velocity = -700
+var gravity = 1500
+var jump_cut_multiplier = 0.6
 
 func _physics_process(delta):
-	# Apply gravity
+	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Left/right movement
+	# Horizontal movement
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * speed
 
-	# Jump
+	# Jump start
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
+
+	# Variable jump height 
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= jump_cut_multiplier
 
 	move_and_slide()
