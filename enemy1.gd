1a09396f98f2c61e6e63671f3ad113c644f68cc8
Andy Ford <andyford1993@gmail.com>
2026-04-21 20:11:22 -0600

    added blocks, moved LR walls, shortened skelleton

--- enemy1.gd
+++ enemy1.gd
@@ -7,22 +7,22 @@ var direction = 1
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	#
-	#if is_on_floor() and abs(velocity.x) > 100:
-		#$AnimatedSprite2D.play("Walk")
-	#else:
-		#$AnimatedSprite2D.play("Idle")
-	#
-	## use direction
-	#velocity.x = direction * SPEED
-	#
-	#move_and_slide()
-	#
-	#for i in range(get_slide_collision_count()):
-		#var collision = get_slide_collision(i)
-		#var normal = collision.get_normal()
-#
-		## turn around on wall hit
-		#if abs(normal.x) > 0.9:
-			#direction *= -1
-			#$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h
+	
+	if is_on_floor() and abs(velocity.x) > 100:
+		$AnimatedSprite2D.play("Walk")
+	else:
+		$AnimatedSprite2D.play("Idle")
+	
+	# use direction
+	velocity.x = direction * SPEED
+	
+	move_and_slide()
+	
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		var normal = collision.get_normal()
+
+		# turn around on wall hit
+		if abs(normal.x) > 0.9:
+			direction *= -1
+			$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h
