a692f9e3f86fe8d6112957e787d7ad1e53f1ba47
Andy Ford <andyford1993@gmail.com>
2026-04-29 14:11:21 -0600

    connected bump logic to enemy, starting to work

--- scripts/world/bump.gd
+++ scripts/world/bump.gd
@@ -18,7 +18,7 @@ func bump(player):
 	is_bumping = true
 	var tween = create_tween()
 	tween.tween_property(self, "position:y", original_y - 20, 0.08)
-	
+	#bumping player
 	var players = get_tree().get_nodes_in_group("player")
 	for p in players:
 		if not is_same(player, p) and p.has_method("handle_bump_stun"):
@@ -28,6 +28,13 @@ func bump(player):
 				else:
 					bump_direction = "left"
 				p.handle_bump_stun(bump_direction)
+	#bumping enemy
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	for e in enemies:
+		print(e)
+		if global_position.distance_to(e.global_position) <= BUMP_RADIUS + 25:
+			e.handle_bump_stun()
+			print("bumped enemy ")
 
 	tween.tween_property(self, "position:y", original_y, 0.12)
 
