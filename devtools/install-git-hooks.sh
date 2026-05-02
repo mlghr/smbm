bc4840096137f0517143ab364fb1d137a9c92a64
Michael Rosenbaum <mrosenbaum.dev@gmail.com>
2026-05-01 17:03:57 -0700

    remove pre commit for now

--- devtools/install-git-hooks.sh
+++ devtools/install-git-hooks.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
-set -euo pipefail
+# #!/usr/bin/env bash
+# set -euo pipefail
 
-cd "$(git rev-parse --show-toplevel)"
+# cd "$(git rev-parse --show-toplevel)"
 
-git config core.hooksPath .githooks
+# git config core.hooksPath .githooks
 
-echo "Git hooks path set to .githooks"
+# echo "Git hooks path set to .githooks"
