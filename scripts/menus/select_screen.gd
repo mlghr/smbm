2b1b766b6f2988513cdca2f95ecac6bda7cbcdea
Michael Rosenbaum <mrosenbaum.dev@gmail.com>
2026-05-27 08:25:40 -0700

    added a pause screen too cause fuck it

--- scripts/menus/select_screen.gd
+++ scripts/menus/select_screen.gd
@@ -8,3 +8,7 @@ func _ready() -> void:
 
 func _on_battle_mode_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/battle_mode_level.tscn")
+
+
+func _on_lava_mode_card_pressed() -> void:
+		get_tree().change_scene_to_file("res://scenes/levels/battle_mode_level.tscn")
